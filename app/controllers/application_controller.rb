ddd71e3feef74e9f8efe830e6a17d47b98e03339
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-03-14 18:19:05 -0500

    update heroku message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: 'Coming soon... Fido and Furball, the greatest thing to ever happen for pets!'
+    render html: 'Coming soon... Fido and Furball, the greatest thing to ever happen for pets! - An Ellie Farmgirl, Mary Nelson and Patrick Hoban Production' 
   end
   
 end
