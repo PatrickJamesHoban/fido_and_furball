33ec8f743e25cbb504c82edca9cbfcd7a5701fcc
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-03-20 16:00:37 -0500

    added dynamic title content to about home help pages and created nav bar

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup 
+    @home_title = "Welcome to Ellie Farmgirl"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@home_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Farmgirl FAQs"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | About Ellie"
   end
 
 end
