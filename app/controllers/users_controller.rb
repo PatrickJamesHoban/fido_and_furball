a437e427140b0880039918b001c6d28573925279
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-05-18 16:55:11 -0500

    Added flash messages for user signup results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Fido & Fluffy!"
+      redirect_to @user
       #TODO add appropriate user save behavior.
     else
       render 'new'
