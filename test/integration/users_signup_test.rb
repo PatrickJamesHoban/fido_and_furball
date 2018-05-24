3eadd7686f6aa6820ba32020d330847cb05efac7
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-05-23 19:49:09 -0500

    Added tests for valid user signup and for flash message upon successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert-danger'
     assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Test User", email: "test@email.com", password: "password", password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
